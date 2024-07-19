ae9b73c1d0840592aa6231f595a90dd420d32543
DLeonardoG <darkleonardog@gmail.com>
2024-07-19 07:36:31 -0500

    feat: :sparkles: Se continuo con la maquetacion
    Se añadieron funcionalidades
    
    @By teamwork

---
+++ importaciones.py
@@ -0,0 +1,11 @@
+from secundary_funciones import *
+from diseños import *
+from txt import *
+from FaixApp_I_S import *
+
+#CRUD Artistas
+# from artist.CRUD import *
+from artist.CRUD import *
+from artist.CRUD_info import *
+
+
