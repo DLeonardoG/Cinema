4792489fd566a43f39c8546293ebb4b352b1ba0e
DLeonardoG <darkleonardog@gmail.com>
2024-07-21 13:31:22 -0500

    fix

--- funciones.py
+++ funciones.py
@@ -6,21 +6,22 @@ def crear_reservas(datos: dict):
     reservas={}
     clear_screen()
     diseño_logo()
-    reservas["cliente_id"]=input("Ingrese el numero de identificación: ").lower()
+    print_ ("F U N C I O N E S  D I S P O N I B L E S")
     for sn in range(len(datos["funciones"])):
         print_(datos["funciones"][sn]["pelicula"].capitalize())
-    funcion = input(("Ingrese la funcion: ").lower())
+    reservas["cliente_id"]=input("Ingrese su numero de identificación: ")
+    funcion = input("Ingrese la funcion que desea: ").lower()
     for i in range(len(datos["funciones"])):
         if datos["funciones"][i]["pelicula"] == funcion:
             reservas["funciones_id"]=datos["funciones"][i]["funciones_id"]
             reservas["pelicula"]=datos["funciones"][i]["pelicula"]
             reservas["sala"]=datos["funciones"][i]["sala"]
             reservas["horario"]=datos["funciones"][i]["horario"]
+            print_(" la reserva de ",reservas["pelicula"]," registrado con éxito!")
             datos["reservas"].append(reservas)
             return datos
         
-    #print(reservas["nombre"]," registrado con éxito!")
-    print("Hola")
+    print("La funcion no se encuentra disponible")
     return datos
 
 def crear_reserva():
@@ -39,6 +40,7 @@ def eliminar_reserva_(datos: dict):
     nombre =input("Ingrese el numero de identificacion: ").lower()
     for i in range(len(datos["reservas"])):
         if datos["reservas"][i]["nombre"] == nombre:
+            
             informacion = (datos["reservas"][i]["pelicula"])
             datos["reservas"].pop(i)
             separador = " "
