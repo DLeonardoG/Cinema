03791d9f46bd0ff7f6f8ba72999dbb97f803c38c
miDaya02 <dannyjair112@gmail.com>
2024-07-21 12:46:40 -0500

    holi
    Co-authored-by: Leonardo Gonzalez <DLeonardoG@users.noreply.github.com>

--- funciones.py
+++ funciones.py
@@ -1,1 +1,58 @@
-import json
+from datos import *
+from funciones_secundarias import *
+from diseños import *
+
+def crear_reservas(datos: dict):
+    reservas={}
+    clear_screen()
+    diseño_logo()
+    reservas["cliente_id"]=input("Ingrese el numero de identificación: ").lower()
+    for sn in range(len(datos["funciones"])):
+        print_(datos["funciones"][sn]["pelicula"].capitalize())
+    funcion = input(("Ingrese la funcion: ").lower())
+    for i in range(len(datos["funciones"])):
+        if datos["funciones"][i]["pelicula"] == funcion:
+            reservas["funciones_id"]=datos["funciones"][i]["funciones_id"]
+            reservas["pelicula"]=datos["funciones"][i]["pelicula"]
+            reservas["sala"]=datos["funciones"][i]["sala"]
+            reservas["horario"]=datos["funciones"][i]["horario"]
+            datos["reservas"].append(reservas)
+            return datos
+        
+    #print(reservas["nombre"]," registrado con éxito!")
+    print("Hola")
+    return datos
+
+def crear_reserva():
+    while True:
+        datos = cargar_datos(RUTA_BASE_DE_DATOS)
+        datos = crear_reservas(datos)
+        guardar_datos(datos, RUTA_BASE_DE_DATOS)
+        continuar = very()
+        if continuar == "2": break
+        else: clear_screen()
+
+crear_reserva()
+
+def eliminar_reserva_(datos: dict):
+    clear_screen()
+    nombre =input("Ingrese el numero de identificacion: ").lower()
+    for i in range(len(datos["reservas"])):
+        if datos["reservas"][i]["nombre"] == nombre:
+            informacion = (datos["reservas"][i]["pelicula"])
+            datos["reservas"].pop(i)
+            separador = " "
+            informacion = separador.join(map(str, informacion))
+            print(informacion," se ha eliminado con exito...")
+            return datos
+    print (f"La reserva de ",nombre," no esta resgistrada...")    
+    return datos
+
+def eliminar_info():
+    while True:
+        datos = cargar_datos(RUTA_BASE_DE_DATOS)
+        datos = eliminar_reserva_(datos)
+        guardar_datos(datos, RUTA_BASE_DE_DATOS)
+        continuar = very()
+        if continuar == "2": break
+        else: clear_screen()
